42f42e2156bb162e0f0798ec82ca75b86ce3a296
User61 <svetsoff@gmail.com>
2022-10-10 08:03:44 +0300

    update

---
+++ js/task-01.js
@@ -0,0 +1,32 @@
+// Завдання 1
+// HTML містить список категорій ul#categories.
+// Напиши скрипт, який:
+
+// Порахує і виведе в консоль кількість категорій в ul#categories, 
+// тобто елементів li.item.
+// Для кожного элемента li.item у спику ul#categories, знайде і 
+// виведе в консоль текст заголовку елемента (тегу <h2>) і кількість 
+// елементів в категорії (усіх <li>, вкладених в нього).
+// В результаті, в консолі будуть виведені наступні повідомлення.
+
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
+
+const categories = document.body.querySelectorAll("li.item");
+console.log("Number of categories:",categories.length);
+console.log("");
+
+categories.forEach(element => {
+    console.log("Category:", element.querySelector("h2").textContent);
+    console.log("Elements:", element.querySelectorAll("li").length);
+    console.log("");
+});
+
--- js/task-03.js
+++ js/task-03.js
@@ -1,3 +1,19 @@
+// Завдання 3
+// Напиши скрипт для створення галереї зображень на підставі масиву даних. 
+// HTML містить список ul.gallery.
+
+// <ul class="gallery"></ul>
+
+// +Використовуй масив об'єктів images для створення елементів <img>, 
+// вкладених в <li>. 
+// +Для створення розмітки використовуй шаблонні рядки 
+// і метод insertAdjacentHTML().
+
+// +Усі елементи галереї повинні додаватися в DOM за одну операцію додавання.
+// Додай мінімальне оформлення галереї флексбоксами або грідами через CSS 
+// класи.
+
+
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -12,3 +28,38 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+//Рішення №1
+const gallery = document.querySelector(".gallery");
+
+const newGallery = [];
+
+images.forEach(({url, alt}) => {
+  const liElem = document.createElement("li");
+  const imgElem = `<img src=${url} alt=${alt} width=540px >`
+  console.log(imgElem);
+  liElem.insertAdjacentHTML("afterbegin", imgElem);
+  newGallery.push(liElem);
+});
+
+gallery.append(...newGallery);
+gallery.classList.add("js-taskThree");
+
+//Рішення №2
+
+// const gallery = document.querySelector(".gallery");
+
+// const newGallery = [];
+
+// images.forEach(({url, alt}) => {
+//   const liElem = document.createElement("li");
+//   const imgElem = document.createElement("img");
+//   imgElem.src = url;
+//   imgElem.alt = alt;
+//   imgElem.width = 540;
+//   liElem.appendChild(imgElem);
+//   newGallery.push(liElem);
+// });
+
+// gallery.append(...newGallery);
+// gallery.classList.add("js-taskThree");

fb73c79e1f9b70f905f7936bdb8c1978fd2271fc
User61 <svetsoff@gmail.com>
2022-10-12 07:58:10 +0300

    update

--- js/task-09.js
+++ js/task-09.js
@@ -1,13 +1,14 @@
 // Завдання 9
-// Напиши скрипт, який змінює кольори фону елемента <body> через інлайн-стиль
-//  по кліку на button.change-color і виводить значення кольору в span.color.
+//+Напиши скрипт, який змінює кольори фону елемента <body> через 
+// інлайн - стиль по кліку на button.change - color і виводить значення 
+// кольору в span.color.
 
 // <div class="widget">
 //   <p>Background color: <span class="color">-</span></p>
 //   <button type="button" class="change-color">Change color</button>
 // </div>
 
-// Для генерування випадкового кольору використовуй функцію getRandomHexColor.
+//+Для генерування випадкового кольору використовуй функцію getRandomHexColor.
 
 // function getRandomHexColor() {
 //   return `#${Math.floor(Math.random() * 16777215)
@@ -19,4 +20,16 @@
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const bodyColor = document.body;
+const buttonChange = document.querySelector(".change-color");
+const colorName = document.querySelector(".color");
+
+buttonChange.addEventListener("click", onClick);
+
+function onClick() {
+  const randomColor = getRandomHexColor();
+  bodyColor.style.background = randomColor;
+  colorName.textContent = randomColor;
 }
--- js/task-10.js
+++ js/task-10.js
@@ -36,3 +36,9 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+const 
+
+console.log(destroyButton);

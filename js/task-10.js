c5332801f83326db6b67e83b67a63bf1431cde3f
User61 <svetsoff@gmail.com>
2022-10-14 20:12:14 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -12,22 +12,22 @@
 
 // <div id="boxes"></div>
 
-// Створи функцію createBoxes(amount), яка приймає один параметр - число. 
-// Функція створює стільки <div>, скільки вказано в amount і додає їх у 
+//+Створи функцію createBoxes(amount), яка приймає один параметр - число. 
+//+Функція створює стільки <div>, скільки вказано в amount і додає їх у 
 // div#boxes.
 
-// Розміри найпершого <div> - 30px на 30px.
-// Кожен елемент після першого повинен бути ширшим і вищим від попереднього 
+//+Розміри найпершого <div> - 30px на 30px.
+//+Кожен елемент після першого повинен бути ширшим і вищим від попереднього 
 // на 10px.
-// Всі елементи повинні мати випадковий колір фону у форматі HEX. 
-// Використовуй готову функцію getRandomHexColor для отримання кольору.
+//+Всі елементи повинні мати випадковий колір фону у форматі HEX. 
+//+Використовуй готову функцію getRandomHexColor для отримання кольору.
 // function getRandomHexColor() {
 //   return `#${Math.floor(Math.random() * 16777215)
 //     .toString(16)
 //     .padStart(6, 0)}`;
 // }
 
-// Створи функцію destroyBoxes(), яка очищає вміст div#boxes, у такий 
+//+Створи функцію destroyBoxes(), яка очищає вміст div#boxes, у такий 
 // спосіб видаляючи всі створені елементи.
 
 
@@ -40,7 +40,30 @@ function getRandomHexColor() {
 const createButton = document.querySelector("[data-create]");
 const destroyButton = document.querySelector("[data-destroy]");
 const countInput = document.querySelector("input");
+const divList = document.querySelector("div#boxes");
+let numberDiv = 0;
+  
+countInput.addEventListener("input", inInput)
+createButton.addEventListener("click", createBoxes);
+destroyButton.addEventListener("click", destroyBoxes);
 
 
+function inInput(event) {
+  numberDiv = event.currentTarget.value;
+}
 
-console.log(countInput);
+function createBoxes() {
+  let divArray = [];
+      for (let index = 0; index < numberDiv; index++) {
+        let divElement = document.createElement("div");
+        let size = 30 + 10 * index + "px";
+      divElement.style.background = getRandomHexColor();
+      divElement.style.width = size;
+      divElement.style.height = size;
+      divArray.push(divElement)
+    }
+  divList.append(...divArray);
+}
+function destroyBoxes() {
+  divList.innerHTML = "";
+}
